55eff3cb2c2b931751e51fdae619b80507d944d3
kisan-79 <kanabarkarkisan56@gmail.com>
2023-05-12 21:23:46 +0530

    new spring boot project

---
+++ src/main/java/com/authotp/AuthOtpApplication.java
@@ -0,0 +1,13 @@
+package com.authotp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthOtpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthOtpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/authotp/AuthOtpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authotp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthOtpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
